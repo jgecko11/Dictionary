91bd105080eb0966c20b5d6b9679c49dc1f21cf6
jgecko11 <jgreco22@kentdenver.org>
2020-12-09 15:01:10 -0700

    instance data/constructor

---
+++ src/Dictionary.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Dictionary<T,E> {
+
+    private ArrayList<E> keyArrL = new ArrayList<E>();
+    private ArrayList<T> valueArrL = new ArrayList<T>();
+    private int size = 0;
+
+    public Dictionary(){
+    }
+
+
+
+
+
+}
